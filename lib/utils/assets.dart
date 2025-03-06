6d2401b6ebaad1bcbd0714a0a955b4bcc8a33eab
mahmoudmohamed17 <ma5975986@gmail.com>
2025-03-06 07:07:22 +0200

    setup some utils

---
+++ lib/utils/assets.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesSplash
+  /// assets/images/splash.png
+  static const String imagesSplash = "assets/images/splash.png";
+
+  /// Assets for imagesTest
+  /// assets/images/test.png
+  static const String imagesTest = "assets/images/test.png";
+}
+
---
+++ lib/utils/styles.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class AppStyles {
+  static const TextStyle bold24 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    fontFamily: 'Poppins',
+  );
+
+  static const TextStyle semiBold20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Poppins',
+  );
+
+  static const TextStyle regular16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Poppins',
+  );
+
+  static const TextStyle regular14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Poppins',
+  );
+
+  static const TextStyle bold18 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+    fontFamily: 'Poppins',
+  );
+}
